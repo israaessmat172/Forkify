0c1884dc8ebd909540c6271529cdf4c499ada3fd
israa <israa@gmail.com>
2023-11-29 01:17:54 +0200

    implementing error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,7 +21,7 @@ const controlRecipes = async function () {
     // Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
