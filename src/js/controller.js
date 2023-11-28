e8469558a5d0176401839257703376ff43844327
israa <israa@gmail.com>
2023-11-29 00:49:29 +0200

    event handlers

--- src/js/controller.js
+++ src/js/controller.js
@@ -21,10 +21,11 @@ const controlRecipes = async function () {
     // Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
