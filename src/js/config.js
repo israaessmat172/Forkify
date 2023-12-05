b6f7a321ae8210ee6b04879925cdbb27a488e7fb
israa <israa@gmail.com>
2023-12-05 03:49:36 +0200

    uploading a new recipe P2

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a88d16e5-d964-426f-ac1c-c2116958b005';
+export const MODAL_CLOSE_SEC = 2.5;
